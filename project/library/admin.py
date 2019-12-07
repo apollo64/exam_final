86b52d7291a3b97b50b5db9e32d3b1e9684d8065
Положенцев Евгений <apollo64@gmail.com>
2019-12-07 15:23:59 +0600

    Самый первый коммит

---
+++ project/library/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from library.models import Author
+
+admin.site.register(Author)
---
+++ project/library/models/Author.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(
+        verbose_name='ФИО',
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+    birth_date = models.DateField(
+        verbose_name='Дата рождения',
+        null=True,
+        blank=True,
+        default=None
+    )
+    death_date = models.DateField(
+        verbose_name='Дата смерти',
+        null=True,
+        blank=True,
+        default=None
+    )
+    biography = models.TextField(
+        verbose_name='Биография',
+        max_length=3000,
+        null=True,
+        blank=True,
+        default=None
+    )
+
+    image = models.ImageField(
+        verbose_name='Фотография',
+        null=True,
+        blank=True,
+        default=None
+    )
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+    def __str__(self):
+        return f'Автор \'{self.name} \''
